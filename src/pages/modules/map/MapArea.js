4c34eb0ff9b4c48f3dd483bc5335ad81f9f7d125
MarianMaschkeRGST <m.maschke@rgst.net>
2022-02-12 16:11:53 +0900

    added map image to to mapContainer

--- src/pages/modules/map/MapArea.js
+++ src/pages/modules/map/MapArea.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../../../css/map.css';
+import MapContainer from './components/MapContainer'
 
 const MapArea = () => {
   return (
-    <div className='map-area'>
+    <div className='map-area fx-col'>
       <h1>I am a map</h1>
+      <MapContainer />
     </div>
   )
 };
---
+++ src/pages/modules/map/components/MapContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MapContainer = () => {
+  return (
+    <div className="map-container">
+        <img src="./img/map/kamolin_map_labeled_v03.jpg" alt="Map of Kamolin" />
+    </div>
+  )
+};
+
+export default MapContainer
